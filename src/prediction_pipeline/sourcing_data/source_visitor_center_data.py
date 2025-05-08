c564c3e274a9fa4d26920ba5fa47f98564911455
Manpa Barman <manpa.barman97@gmail.com>
2025-05-08 09:26:52 +0200

    fixed wrong filename

--- src/prediction_pipeline/sourcing_data/source_visitor_center_data.py
+++ src/prediction_pipeline/sourcing_data/source_visitor_center_data.py
@@ -1,10 +1,8 @@
-import awswrangler as wr
 import pandas as pd
-from src.config import aws_s3_bucket
 import os
 
 visitor_center_data_path = os.path.join("data","raw","visitor_center","national-park-vacation-times-houses-opening-times-visitors.xlsx")
-saved_path_visitor_center_query = os.path.join("data","processed","visitor_center","visitor_centers_2017_to_2024.parquet")
+saved_path_visitor_center_modelling = os.path.join("data","processed","visitor_center","visitor_centers_hourly.parquet")
 
 def source_visitor_center_data():
     # Source data - this is the preprocessed data
@@ -15,10 +13,10 @@ def source_visitor_center_data():
 def source_preprocessed_hourly_visitor_center_data():
 
     """
-    Load the preprocessed hourly visitor center data from AWS S3.
+    Load the preprocessed hourly visitor center data .
     """
 
     # Load visitor count data from AWS S3
-    preprocessed_hourly_visitor_center_data = pd.read_parquet(saved_path_visitor_center_query)
+    preprocessed_hourly_visitor_center_data = pd.read_parquet(saved_path_visitor_center_modelling)
 
     return preprocessed_hourly_visitor_center_data

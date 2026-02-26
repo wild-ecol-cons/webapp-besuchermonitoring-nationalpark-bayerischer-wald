ff52f911608f08aa236bfddbe1efc2fb104fb2cd
Julia Ostheimer <42974945+JOPloume@users.noreply.github.com>
2026-02-26 19:56:24 +0100

    Add Duck DB functions to utils script

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
+import duckdb
 import pandas as pd
-from src.config import CONTAINER_NAME, storage_options, CONNECTION_STRING
-from typing import Dict, Any, Optional
-from azure.storage.blob import BlobServiceClient
 import streamlit as st
+
+from src.config import CONTAINER_NAME, storage_options, CONNECTION_STRING
+from typing import Dict, Any, Optional, List
+from azure.storage.blob import BlobServiceClient
+
 
 def read_dataframe_from_azure(
     file_name: str,
@@ -189,3 +192,73 @@ def upload_file_to_azure(file_obj: object, target_folder: str, filename: str) ->
     except Exception as e:
         st.error(f"An error occurred when trying to upload the file: {e}")
         return False
+    
+
+def set_up_duck_db_connection() -> duckdb.DuckDBPyConnection:
+    """
+    Set up a DuckDB connection via files on Azure Blob Storage to a transient in-memory database. This connection is used to query data with SQL queries.
+
+    Returns:
+        duckdb.DuckDBPyConnection: A DuckDB connection
+    """
+    # Connect to a transient in-memory DuckDB
+    conn = duckdb.connect(database=':memory:')
+
+    # Install and load the Azure extension (one-time per session)
+    conn.execute("INSTALL azure; LOAD azure;")
+
+    # Authenticate with Azure
+    secret_query = f"""
+    CREATE OR REPLACE SECRET (
+        TYPE AZURE,
+        CONNECTION_STRING '{CONNECTION_STRING}'
+    );
+    """
+    conn.execute(secret_query)
+
+    # Set parameter to solve certificate issue
+    conn.execute("SET azure_transport_option_type = 'curl';")
+    return conn
+
+def query_azure_with_duck_db(
+    directory: str,
+    columns: List[str] = ["*"],
+    filters: Optional[str] = None,
+    limit: Optional[int] = 10
+) -> pd.DataFrame:
+    """
+    Queries Parquet files on Azure with optional filtering and selection.
+
+    Args:
+        directory (str): The directory path within the container
+        columns (List[str], optional): A list of column names to select. Defaults to ["*"].
+        filters (Optional[str], optional): An SQL WHERE clause to apply to the query. Defaults to None.
+        limit (Optional[int], optional): The maximum number of rows to return. Defaults to 10.
+
+    Returns:
+        pd.DataFrame: A Pandas DataFrame containing the results of the query
+    """
+    conn = set_up_duck_db_connection()
+    
+    # 1. Construct the column string
+    col_selector = ", ".join(columns)
+    
+    # 2. Base URL
+    path = f"az://webapp-besuchermonitoring-data-dev/{directory}/*.parquet"
+    
+    # 3. Build the SQL string dynamically
+    query = f"SELECT {col_selector} FROM read_parquet('{path}')"
+    
+    # 4. Append WHERE clause if filters are provided
+    if filters:
+        query += f" WHERE {filters}"
+    
+    # 5. Append LIMIT
+    if limit:
+        query += f" LIMIT {limit}"
+    
+    try:
+        return conn.execute(query).df()
+    except Exception as e:
+        print(f"Query failed: {e}")
+        return pd.DataFrame()

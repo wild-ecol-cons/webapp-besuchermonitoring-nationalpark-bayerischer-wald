6b68daf3108ead0e0e807ea1fa110786934f2f2e
Julia Ostheimer <42974945+JOPloume@users.noreply.github.com>
2026-04-19 16:09:13 +0200

    Fix bug to rename all sensors with Ä in Schwellhäusel

--- src/config.py
+++ src/config.py
@@ -30,7 +30,7 @@ regions = {
     'Bayerischer Wald Total': ['sum_IN_abs', 'sum_OUT_abs'],
     'Nationalparkzentrum Falkenstein': ['Nationalparkzentrum Falkenstein IN', 'Nationalparkzentrum Falkenstein OUT'],
     'Nationalparkzentrum Lusen': ['Nationalparkzentrum Lusen IN', 'Nationalparkzentrum Lusen OUT'],
-    'Falkenstein-Schwellhäusl': ['Falkenstein-Schwellhausl IN', 'Falkenstein-Schwellhausl OUT'],
+    'Falkenstein-Schwellhäusl': ['Falkenstein-Schwellhäusl IN', 'Falkenstein-Schwellhäusl OUT'],
     'Scheuereck-Schachten-Trinkwassertalsperre': ['Scheuereck-Schachten-Trinkwassertalsperre IN', 'Scheuereck-Schachten-Trinkwassertalsperre OUT'],
     'Lusen-Mauth-Finsterau': ['Lusen-Mauth-Finsterau IN', 'Lusen-Mauth-Finsterau OUT'],
     'Rachel-Spiegelau': ['Rachel-Spiegelau IN', 'Rachel-Spiegelau OUT'],

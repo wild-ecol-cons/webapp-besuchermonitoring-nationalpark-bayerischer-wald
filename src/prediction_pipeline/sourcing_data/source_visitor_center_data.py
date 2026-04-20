967190603585e4ccfad0df7227425c82a0059137
Julia Ostheimer <42974945+JOPloume@users.noreply.github.com>
2026-04-20 10:42:02 +0200

    Remove previously one-time preprocessed temporal features
    Instead: always freshly retrieve data from OpenHolidaysAPI

--- src/prediction_pipeline/sourcing_data/source_visitor_center_data.py
+++ src/prediction_pipeline/sourcing_data/source_visitor_center_data.py
@@ -78,22 +78,3 @@ def source_temporal_features(
     )
 
     return fetched_vacation_df
-
-def source_preprocessed_hourly_visitor_center_data():
-
-    """
-    Load the preprocessed hourly visitor center data from the cloud.
-    """
-
-    print("Sourcing the historic preprocessed_hourly_visitor_center_data")
-
-    # Load visitor count data from the cloud
-    preprocessed_hourly_visitor_center_data = read_dataframe_from_azure(
-        file_name="visitor_centers_hourly_2017_to_2026.parquet",
-        file_format="parquet",
-        source_folder="preprocessed_data",
-    )
-
-    print(f"The historic preprocessed_hourly_visitor_center_data is: {preprocessed_hourly_visitor_center_data}")
-
-    return preprocessed_hourly_visitor_center_data
